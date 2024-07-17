389c22a35c6fae8d35bf84e2254201df3f067ff5
ash991213 <ads1213@nate.com>
2024-07-17 16:48:13 +0900

    feat : ConfigModule 동적 모듈로 변경

--- libs/module/config/config.module.ts
+++ libs/module/config/config.module.ts
@@ -1,23 +1,35 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { IEnvConfigService } from '@libs/module/config/adapter';
 import { EnvConfigService } from '@libs/module/config/service';
 
-@Module({
-    imports: [
-        ConfigModule.forRoot({
-            envFilePath: `@libs/module/config/env/${process.env.NODE_ENV === 'prod' ? '.env.prod' : process.env.NODE_ENV === 'test' ? '.env.test' : '.env.dev'}`,
-            isGlobal: true,
-            cache: true,
-        }),
-    ],
-    providers: [
-        {
-            provide: IEnvConfigService,
-            useClass: EnvConfigService,
-        },
-    ],
-    exports: [IEnvConfigService],
-})
-export class EnvConfigModule {}
+@Module({})
+export class EnvConfigModule {
+    static forRoot(additionalEnvPaths: string[] = [], ICLS, CLS): DynamicModule {
+        const defaultEnvPath = `@libs/module/config/env/${process.env.NODE_ENV === 'prod' ? '.env.prod' : process.env.NODE_ENV === 'test' ? '.env.test' : '.env.dev'}`;
+        const envFilePaths = [defaultEnvPath, ...additionalEnvPaths];
+
+        return {
+            module: EnvConfigModule,
+            imports: [
+                ConfigModule.forRoot({
+                    envFilePath: envFilePaths,
+                    isGlobal: true,
+                    cache: true,
+                }),
+            ],
+            providers: [
+                {
+                    provide: IEnvConfigService,
+                    useClass: EnvConfigService,
+                },
+                {
+                    provide: ICLS,
+                    useClass: CLS,
+                },
+            ],
+            exports: [IEnvConfigService],
+        };
+    }
+}
