6587e83ca92fdd7eb0758393f5187582af6cec8d
안수환 <ansuhwan@ansuhwan-ui-MacBookAir.local>
2024-07-28 21:43:56 +0900

    feat : payment 서비스 entity 추가

---
+++ apps/payment/src/payment/domain/entity/payment.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'payment' })
+export class PaymentEntity {
+    @PrimaryGeneratedColumn({ type: 'int', comment: '결제 기본키' })
+    payment_id: number;
+
+    @Column({ type: 'int', comment: '회원 기본키', nullable: false })
+    user_id: number;
+
+    @Column({ type: 'int', comment: '점주 기본키', nullable: false })
+    host_id: number;
+
+    @CreateDateColumn({ type: 'datetime', comment: '생성 시간', nullable: false })
+    payment_date: Date;
+
+    @Column({ type: 'decimal', comment: '결제 가격', nullable: false })
+    payment_price: number;
+
+    @Column({ type: 'enum', comment: '결제 상태', nullable: false })
+    payment_status;
+
+    @Column({ type: 'varchar', comment: '결제 메시지', length: 300, default: null, nullable: true })
+    payment_message: string;
+
+    @Column({ type: 'varchar', comment: '결제 ID', length: 128, default: null, nullable: true })
+    payment_transaction_id: string;
+
+    @CreateDateColumn({ type: 'datetime', comment: '생성 시간', nullable: false })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: 'datetime', comment: '변경 시간', nullable: false })
+    updated_at: Date;
+}
