46b5e16e41d050cc10fa03df939cf87ce0dae6df
ash991213 <ads1213@nate.com>
2024-07-29 13:53:05 +0900

    feat : noti enity enum 추가

--- apps/noti/src/noti/domain/entity/noti.entity.ts
+++ apps/noti/src/noti/domain/entity/noti.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { NOTIFICATION_CHANNEL, NOTIFICATION_STATUS, NOTIFICATION_TYPE, RECIPIENT_TYPE } from '@apps/noti/src/noti/domain/enum/noti.enum';
 
 @Entity({ name: 'notification' })
 export class NotiEntity {
@@ -6,7 +7,7 @@ export class NotiEntity {
     noti_id: number;
 
     @Column({ type: 'varchar', comment: '받는 회원 타입', default: null, nullable: true })
-    recipient_type;
+    recipient_type: RECIPIENT_TYPE;
 
     @Column({ type: 'bool', comment: '전체 전송 여부', default: null, nullable: true })
     recipient_is_all: boolean;
@@ -15,16 +16,16 @@ export class NotiEntity {
     recipient_id: number;
 
     @Column({ type: 'enum', comment: '알림 채널', nullable: false })
-    notification_channel;
+    notification_channel: NOTIFICATION_CHANNEL;
 
     @Column({ type: 'enum', comment: '알림 타입', nullable: false })
-    notification_type;
+    notification_type: NOTIFICATION_TYPE;
 
     @Column({ type: 'text', comment: '알림 내용', nullable: false })
     notification_content: string;
 
     @Column({ type: 'enum', comment: '알림 상태', nullable: false })
-    notification_status;
+    notification_status: NOTIFICATION_STATUS;
 
     @Column({ type: 'varchar', comment: '알림 실패 시 사유', length: 300, default: null, nullable: true })
     notification_message: string;
--- apps/noti/src/noti/domain/enum/noti.enum.ts
+++ apps/noti/src/noti/domain/enum/noti.enum.ts
@@ -1,7 +1,24 @@
-export enum RECIPIENT_TYPE {}
+export enum RECIPIENT_TYPE {
+    USER = Symbol['USER'],
+    HOST = Symbol['HOST'],
+    ADMIN = Symbol['ADMIN'],
+}
 
-export enum NOTIFICATION_CHANNEL {}
+export enum NOTIFICATION_CHANNEL {
+    EMAIL = Symbol['EMAIL'],
+    SLACK = Symbol['SLACK'],
+}
 
-export enum NOTIFICATION_TYPE {}
+export enum NOTIFICATION_TYPE {
+    PRODUCT = Symbol['PRODUCT'],
+    ORDER = Symbol['ORDER'],
+    SHIPMENT = Symbol['SHIPMENT'],
+    PAYMENT = Symbol['PAYMENT'],
+    ETC = Symbol['ETC'],
+}
 
-export enum NOTIFICATION_STATUS {}
+export enum NOTIFICATION_STATUS {
+    PENDING = Symbol['PENDING'],
+    SENT = Symbol['SENT'],
+    FAILED = Symbol['FAILED'],
+}
