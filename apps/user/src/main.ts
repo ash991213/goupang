dc1f8dc293530637b3adb3f50828092bb1edb932
ash991213 <ads1213@nate.com>
2024-08-05 15:55:12 +0900

    feat : user app module 추가

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { UserModule } from '@apps/user/src/user/user.module';
+import { AppModule } from '@apps/user/src/app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(UserModule);
+    const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
 bootstrap();
