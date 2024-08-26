c620ae3bf44c166268b2d7493cc23261dbcfbcbe
ash991213 <ads1213@nate.com>
2024-08-26 16:41:37 +0900

    chore : deploy 테스트

--- apps/user/src/user/infrastructure/inbound/port/user.service.ts
+++ apps/user/src/user/infrastructure/inbound/port/user.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
     getHello(): string {
-        return 'User Workflow Test!!';
+        return 'User Workflow Test!';
     }
 }
