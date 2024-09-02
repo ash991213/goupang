506ff5d6ad5e407201fd303c3553acf4b9f7c4ca
ash991213 <ads1213@nate.com>
2024-09-02 16:21:43 +0900

    fix : deploy script docker service 실행 로직 수정

--- apps/user/scripts/pull_and_run.sh
+++ apps/user/scripts/pull_and_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SERVICES=("goupang-user" "master-node" "slave-node1" "slave-node2" "nginx")
+
 SERVICE_NAME="goupang-user"
 ENV_FILE="/home/ubuntu/$SERVICE_NAME/env/.env.prod"
 DOCKER_COMPOSE_FILE="/home/ubuntu/$SERVICE_NAME/docker-compose.yml"
@@ -8,16 +10,23 @@ IMAGE_NAME="$IMAGE_PATH/goupang/user:latest"
 
 LOGIN_CMD="aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $IMAGE_PATH"
 
-if docker ps --format '{{.Names}}' | grep -q "^$SERVICE_NAME$"; then
-    echo "Container $SERVICE_NAME exists. Pulling the latest image and restarting the service."
+for service in "${SERVICES[@]}"; do
 
-    eval $LOGIN_CMD
+    CONTAINER_NAME="${service}"
+    
+    if [ "$service" == "$SERVICE_NAME" ]; then
+        eval $LOGIN_CMD
 
-    docker pull $IMAGE_NAME
+        echo "Updating $SERVICE_NAME with the latest image."
+        docker pull $IMAGE_NAME
 
-    docker-compose --env-file $ENV_FILE -f $DOCKER_COMPOSE_FILE -p $SERVICE_NAME up -d $SERVICE_NAME
-else
-    echo "Container $SERVICE_NAME does not exist. Bringing up all services."
-
-    docker-compose --env-file $ENV_FILE -f $DOCKER_COMPOSE_FILE -p $SERVICE_NAME up -d
-fi
+        docker-compose --env-file $ENV_FILE -f $DOCKER_COMPOSE_FILE -p $SERVICE_NAME up -d --force-recreate $SERVICE_NAME
+    else
+        if docker ps --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
+            echo "Service $service is already running. Skipping."
+        else
+            echo "Service $service is not running. Starting."
+            docker-compose --env-file $ENV_FILE -f $DOCKER_COMPOSE_FILE -p $SERVICE_NAME up -d $service
+        fi
+    fi
+done
