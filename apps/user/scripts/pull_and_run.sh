23e1f3740fb043d9b831b51539135bc2b27b8b97
ash991213 <ads1213@nate.com>
2024-09-02 11:47:22 +0900

    fix : deploy script service 실행 명령어 수정

--- apps/user/scripts/pull_and_run.sh
+++ apps/user/scripts/pull_and_run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ENV_FILE="/home/ubuntu/goupang-user/env/.env.prod"
-
 PROJECT_NAME="goupang-user"
-
 SERVICES=("master-node" "slave-node1" "slave-node2" "goupang-user" "nginx")
 
 RUNNING_CONTAINERS=$(docker ps --format '{{.Names}}')
 
-CMD="docker-compose --env-file $ENV_FILE -p $PROJECT_NAME up -d"
+CMD="docker-compose --env-file $ENV_FILE -p $PROJECT_NAME"
 
 for SERVICE in "${SERVICES[@]}"; do
     if [[ "$SERVICE" == "goupang-user" || ! $RUNNING_CONTAINERS =~ $SERVICE ]]; then
         CMD+=" $SERVICE"
     fi
 done
+
+CMD+="up -d"
 
 echo "Executing command: $CMD"
 eval $CMD
