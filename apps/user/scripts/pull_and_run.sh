ade22fdafe5aa7f742a031cec5aa26008bb66efc
ash991213 <ads1213@nate.com>
2024-09-02 14:45:12 +0900

    feat : deploy script docker image pull 로직 추가

--- apps/user/scripts/pull_and_run.sh
+++ apps/user/scripts/pull_and_run.sh
@@ -2,12 +2,15 @@
 
 ENV_FILE="/home/ubuntu/goupang-user/env/.env.prod"
 DOCKER_COMPOSE_FILE="/home/ubuntu/goupang-user/docker-compose.yml"
+IMAGE_NAME="262872842537.dkr.ecr.ap-northeast-2.amazonaws.com/goupang/user:latest"
 
 PROJECT_NAME="goupang-user"
 
 SERVICES=("master-node" "slave-node1" "slave-node2" "goupang-user" "nginx")
 
 RUNNING_CONTAINERS=$(docker ps --format '{{.Names}}')
+
+docker pull $IMAGE_NAME
 
 CMD="docker-compose --env-file $ENV_FILE -f $DOCKER_COMPOSE_FILE -p $PROJECT_NAME up -d"
 
