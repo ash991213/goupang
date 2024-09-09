201577579d30fd2e0dc6a4e36a6bd48073ed39d4
ash991213 <ads1213@nate.com>
2024-09-09 16:48:36 +0900

    fix : deploy main.yml create .env 순서 변경

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from '@apps/user/src/app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    console.log('port :', process.env.PORT);
     await app.listen(process.env.PORT);
 }
 bootstrap();
