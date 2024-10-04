3c8f645ab2e011794ca9c1d4276b96d963b59b51
ash991213 <ads1213@nate.com>
2024-10-04 14:35:50 +0900

    fix : noti service 환경 변수 추가

--- apps/noti/src/main.ts
+++ apps/noti/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { NotiModule } from '@apps/noti/src/noti/noti.module';
+import { AppModule } from '@apps/noti/src/app.module';
+import { IEnvConfigService } from '@libs/module/config/adapter';
 
 async function bootstrap() {
-    const app = await NestFactory.create(NotiModule);
-    await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const configService = app.get(IEnvConfigService, { strict: false });
+    await app.listen(configService.PORT.PRODUCT_PORT);
 }
 bootstrap();
