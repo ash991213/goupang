e1ad65c8b6bee091ce70d7feb87b85de4d6ae63e
ash991213 <ads1213@nate.com>
2024-12-10 11:45:59 +0900

    feat : db module, config module 추가

--- apps/shipment/src/app.module.ts
+++ apps/shipment/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ShipmentModule } from '@apps/shipment/src/shipment/shipment.module';
 import { EnvConfigModule } from '@libs/module/config/config.module';
+import { DatabaseModule } from '@libs/module/database/database.module';
 
 @Module({
-    imports: [ShipmentModule, EnvConfigModule.forRoot([`apps/shipment/env/${process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev'}`])],
+    imports: [ShipmentModule, EnvConfigModule, DatabaseModule],
     providers: [],
 })
 export class AppModule {}
--- apps/shipment/src/shipment/shipment.module.ts
+++ apps/shipment/src/shipment/shipment.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { EnvConfigModule } from '@libs/module/config/config.module';
+import { DatabaseModule } from '@libs/module/database/database.module';
+
 import { ShipmentController } from '@apps/shipment/src/shipment/infrastructure/inbound/adapter/shipment.controller';
 import { ShipmentService } from '@apps/shipment/src/shipment/infrastructure/inbound/port/shipment.service';
 
 @Module({
-    imports: [],
+    imports: [EnvConfigModule.forRoot([`apps/shipment/env/${process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev'}`]), DatabaseModule.forRootAsync([])],
     controllers: [ShipmentController],
     providers: [ShipmentService],
 })
