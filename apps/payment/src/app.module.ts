5b04066b8ec57086a8c6f29e60e2578a1edb68bb
ash991213 <ads1213@nate.com>
2024-12-10 11:39:19 +0900

    feat : db module, config module 추가

--- apps/payment/src/app.module.ts
+++ apps/payment/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentModule } from '@apps/payment/src/payment/payment.module';
 import { EnvConfigModule } from '@libs/module/config/config.module';
+import { DatabaseModule } from '@libs/module/database/database.module';
 
 @Module({
-    imports: [PaymentModule, EnvConfigModule.forRoot([`apps/payment/env/${process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev'}`])],
+    imports: [PaymentModule, EnvConfigModule, DatabaseModule],
     providers: [],
 })
 export class AppModule {}
