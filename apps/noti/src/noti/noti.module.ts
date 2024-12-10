2c8773dc4c0a728c116825c134f9c7ef1eb196d5
ash991213 <ads1213@nate.com>
2024-12-10 11:55:47 +0900

    fix : import 수정

--- apps/noti/src/noti/noti.module.ts
+++ apps/noti/src/noti/noti.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { NotiController } from '@apps/noti/src/noti/infrastructure/inbound/adapter/noti.controller';
-import { NotiService } from '@apps/noti/src/noti/infrastructure/inbound/port/noti.service';
 import { EnvConfigModule } from '@libs/module/config/config.module';
 import { DatabaseModule } from '@libs/module/database/database.module';
+
+import { NotiController } from '@apps/noti/src/noti/infrastructure/inbound/adapter/noti.controller';
+import { NotiService } from '@apps/noti/src/noti/infrastructure/inbound/port/noti.service';
 
 @Module({
     imports: [EnvConfigModule.forRoot([`apps/noti/env/${process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev'}`]), DatabaseModule.forRootAsync([])],
