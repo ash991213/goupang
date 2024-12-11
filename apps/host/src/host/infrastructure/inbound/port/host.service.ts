57fcd03d8db7555176a0205efa7aa3335d60f06b
ash991213 <ads1213@nate.com>
2024-12-11 16:55:22 +0900

    fix : host workflow copy env 오타 수정

--- apps/host/src/host/infrastructure/inbound/port/host.service.ts
+++ apps/host/src/host/infrastructure/inbound/port/host.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HostService {
     getHello(): string {
-        return 'Host Workflow Test!!';
+        return 'Host Workflow Test!';
     }
 }
