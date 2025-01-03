9cf5eb5824ff31e7519fd5ef5e02c25be04eee24
winterm1 <oiool@naver.com>
2025-01-03 15:59:52 +0900

    feat: kafka config 종속성 수정

--- libs/module/kafka/kafka.module.ts
+++ libs/module/kafka/kafka.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from '@nestjs/microservices';
-import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
-import { KafkaConfigService } from './kafka.service';
+import { EnvConfigModule } from '@libs/module/config/config.module';
+import { KafkaConfigService } from '@libs/module/kafka/kafka.service';
 
 @Module({
     imports: [
-        NestConfigModule,
         ClientsModule.registerAsync([
             {
                 name: 'KAFKA_SERVICE',
+                imports: [EnvConfigModule.forRoot()],
                 extraProviders: [KafkaConfigService],
                 inject: [KafkaConfigService],
                 useFactory: (kafkaConfigService: KafkaConfigService) => kafkaConfigService.getKafkaConfig(),
